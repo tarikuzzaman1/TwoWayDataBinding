d9635c0ec691105fbfa0420c791f2bed462b422c
ASUS <tarikuzzaman1@gmail.com>
2020-01-18 22:15:23 +0600

    Initial commit

---
+++ app/src/main/java/bd/dev/tarik/example/twowaydatabinding/Contact.java
@@ -0,0 +1,47 @@
+package bd.dev.tarik.example.twowaydatabinding;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class Contact extends BaseObservable {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public Contact(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Bindable
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+        //notifyPropertyChanged(androidx.databinding.library.baseAdapters.BR.);
+        notifyPropertyChanged(BR.id);
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+    @Bindable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
+}
